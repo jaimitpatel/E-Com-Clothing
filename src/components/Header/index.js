057d6f7eb27ea7674f65d170a22c76136c521a40
Jaimit Patel <jaimit_12@hotmail.com>
2022-04-24 18:17:57 -0400

    logo update

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './styles.scss';
-import Logo from './../../assets/logo.png';
+import Logo from './../../assets/logo2.png';
 
 const Header = () => {
     return (
