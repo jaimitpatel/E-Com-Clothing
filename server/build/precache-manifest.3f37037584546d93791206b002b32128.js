cba521a03ed0fde95f82b78b61965f01244c75fa
Jaimit Patel <jaimit_12@hotmail.com>
2022-04-28 15:21:08 -0400

    building application

---
+++ server/build/precache-manifest.3f37037584546d93791206b002b32128.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ac6053b73cc1dbc5a7a909b18bbbdf1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0f13fb5fcd006817df2",
+    "url": "/static/css/main.740fb8d6.chunk.css"
+  },
+  {
+    "revision": "2dece547585a8499003e",
+    "url": "/static/js/2.974ae7ef.chunk.js"
+  },
+  {
+    "revision": "993331370d75f6cc96f54a5ac31490a3",
+    "url": "/static/js/2.974ae7ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0f13fb5fcd006817df2",
+    "url": "/static/js/main.a28e4449.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
