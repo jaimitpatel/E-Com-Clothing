92f1d7bc719fa770fe68c367a6786b520200206f
Jaimit Patel <jaimit_12@hotmail.com>
2022-04-28 02:04:16 -0400

    removing some logs

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/jaimitsclothing',
+    process.env.MONGODB_URI || 'mongodb://localhost:27017/jamiesclothing',
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
