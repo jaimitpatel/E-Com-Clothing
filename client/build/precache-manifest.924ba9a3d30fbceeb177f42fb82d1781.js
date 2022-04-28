196fb5e3ee234e3f89d47005dcd4521ce1c43336
Jaimit Patel <jaimit_12@hotmail.com>
2022-04-27 23:47:25 -0400

    bug fixes

---
+++ client/build/precache-manifest.924ba9a3d30fbceeb177f42fb82d1781.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71167a1e517147f800dacf641222a168",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0149b92aaf2cdce4b9f3",
+    "url": "/static/css/main.0c81070a.chunk.css"
+  },
+  {
+    "revision": "2dece547585a8499003e",
+    "url": "/static/js/2.974ae7ef.chunk.js"
+  },
+  {
+    "revision": "993331370d75f6cc96f54a5ac31490a3",
+    "url": "/static/js/2.974ae7ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0149b92aaf2cdce4b9f3",
+    "url": "/static/js/main.419d0e7a.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -2,49 +2,49 @@ import React from 'react';
 import Auth from '../../utils/auth';
 import { Link } from 'react-router-dom';
 
-function Nav() {
-  function showNavigation() {
-    if (Auth.loggedIn()) {
-      return (
-        <ul className='navbar navbar-expand-lg navbar-light bg-light'>
-          <li className='mx-1'>
-            <Link to='/orderHistory'>Order History</Link>
-          </li>
-          <li className='mx-1'>
-            {/* this is not using the Link component to logout or user and then refresh the application to the start */}
-            <a href='/' onClick={() => Auth.logout()}>
-              Logout
-            </a>
-          </li>
-        </ul>
-      );
-    } else {
-      return (
-        <ul className='flex-row'>
-          <li className='mx-1'>
-            <Link to='/signup'>Signup</Link>
-          </li>
-          <li className='nav-item nav-link'>
-            <Link to='login' className='btn btn-outline-dark m-2'>
-              <i className='fa fa-sign-in'></i> Login
-            </Link>{' '}
-          </li>
-        </ul>
-      );
+const Navbar = () => {
+    function showNavigation() {
+        if (Auth.loggedIn()) {
+            return (
+                <ul className="navbar navbar-expand-lg navbar-light bg-light">
+                    <li className="mx-1">
+                        <Link to="/orderHistory">Order History</Link>
+                    </li>
+                    <li className="mx-1">
+                        {/* this is not using the Link component to logout or user and then refresh the application to the start */}
+                        <a href="/" onClick={() => Auth.logout()}>
+                            Logout
+                        </a>
+                    </li>
+                </ul>
+            );
+        } else {
+            return (
+                <ul className="flex-row">
+                    <li className="mx-1">
+                        <Link to="/signup">Signup</Link>
+                    </li>
+                    <li className="nav-item nav-link">
+                        <Link to="login" className="btn btn-outline-dark m-2">
+                            <i className="fa fa-sign-in"></i> Login
+                        </Link>{' '}
+                    </li>
+                </ul>
+            );
+        }
     }
-  }
 
-  return (
-    <header className='flex-row px-1'>
-      <h1>
-        <Link className='navbar-brand' to='/'>
-          Jamie's
-        </Link>
-      </h1>
+    return (
+        <header className="flex-row px-1">
+            <h1>
+                <Link className="navbar-brand" to="/">
+                    Jamie's
+                </Link>
+            </h1>
 
-      <nav>{showNavigation()}</nav>
-    </header>
-  );
-}
+            <nav>{showNavigation()}</nav>
+        </header>
+    );
+};
 
-export default Nav;
+export default Navbar;
